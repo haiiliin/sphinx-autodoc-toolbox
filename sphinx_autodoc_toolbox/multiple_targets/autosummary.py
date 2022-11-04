c478ea28902c9cc6a1b5a2cb31f71d2a69bd3734
Hailin-Wang <hailin.wang@connect.polyu.hk>
2022-11-04 15:25:54 +0800

    Update autosummary label

--- sphinx_autodoc_toolbox/multiple_targets/autosummary.py
+++ sphinx_autodoc_toolbox/multiple_targets/autosummary.py
@@ -6,24 +6,20 @@ from sphinx.ext.autosummary import Autosummary as SphinxAutosummary
 
 class Autosummary(SphinxAutosummary):
     separator = "|"
-    separator_summary = "->"
+    separator_label = ":"
 
     def get_items(self, names: List[str]) -> List[Tuple[str, str, str, str]]:
         items = []
         for name in names:
-            summary_name = None
-            if self.separator_summary in name:
-                summary_name, name = re.split(rf"[{self.separator_summary}\s]+", name, 1)
-            items.append(self.get_summary_item(super().get_items(re.split(rf"[{self.separator}\s]+", name)),
-                                               summary_name))
+            label = None
+            if self.separator_label in name:
+                name, label = re.split(rf"[{self.separator_label}\s]+", name, 1)
+            items.append(self.get_summary_item(super().get_items(re.split(rf"[{self.separator}\s]+", name)), label))
         return items
 
-    def get_summary_item(self, items: List[Tuple[str, str, str, str]], summary_name: str) -> Tuple[str, str, str, str]:
+    def get_summary_item(self, items: List[Tuple[str, str, str, str]], label: str) -> Tuple[str, str, str, str]:
         if len(items) == 1:
             return items[0]
         real_name = self.separator.join([item[3] for item in items])
         display_name = self.separator.join([item[0] for item in items])
-        for index, (_, _, _, name) in enumerate(items):
-            if summary_name and summary_name in name:
-                return items[index][0], items[index][1], items[index][2], real_name
-        return display_name, items[0][1], items[0][2], real_name
+        return label or display_name, items[0][1], items[0][2], real_name
