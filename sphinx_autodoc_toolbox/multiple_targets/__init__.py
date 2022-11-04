341c08318616b6e197b6af83624e3b8b7c0f7b88
Hailin-Wang <hailin.wang@connect.polyu.hk>
2022-11-04 19:23:50 +0800

    Update __init__.py

--- sphinx_autodoc_toolbox/multiple_targets/__init__.py
+++ sphinx_autodoc_toolbox/multiple_targets/__init__.py
@@ -6,15 +6,15 @@ from sphinx.domains.python import filter_meta_fields, builtin_resolver
 from sphinx.ext.autosummary import process_generate_options
 
 from .autosummary import Autosummary
-from.pydomain import PythonDomain
+from .pydomain import PythonDomain
 
 
 def setup(app: Sphinx) -> Dict[str, Any]:
     app.setup_extension('sphinx.ext.autodoc')
     app.add_directive('autosummary', Autosummary)
     app.connect('builder-inited', process_generate_options)
-    app.setup_extension("sphinx.directives")
 
+    app.setup_extension("sphinx.directives")
     app.add_domain(PythonDomain, override=True)
     app.connect("object-description-transform", filter_meta_fields)
     app.connect("missing-reference", builtin_resolver, priority=900)
@@ -25,4 +25,3 @@ def setup(app: Sphinx) -> Dict[str, Any]:
         "parallel_read_safe": True,
         "parallel_write_safe": True,
     }
-
